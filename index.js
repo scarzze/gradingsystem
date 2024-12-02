57a81879dc785f725fe9e87541055a1cb88dbed3
Hosea biwott <hoseabiwott8@gmail.com>
2024-12-02 18:58:11 +0300

    re-do
    re-done after previous loss of data

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-function checkSpeed(speed) {
-    if (speed < 70) {
-        console.log("Ok");
-    } else {
-        const excess = speed - 70;
-        let points = Math.floor(excess / 5);
-        
-        if (points > 12) {
-            console.log("License suspended");
-        } else {
-            console.log("Points: " + points);
+function getStudentGrade() {
+let marks;
+while (true) {
+// Prompt the user for input
+        marks = prompt("Enter student marks (0 to 100):");
+
+ // Convert input to percent
+        marks = Number(marks);
+
+ // Check if the input is a valid number
+    if (isNaN(marks) || marks < 0 || marks > 100) {
+        console.log(
+        "Please enter a valid mark between 0 and 100.");
         }
+// Determine the grade as per scores
+    let grade;
+        if (marks > 79) {
+            grade = 'A';
+        } else if (marks >= 60) {
+            grade = 'B';
+        } else if (marks >= 50) {
+            grade = 'C';
+        } else if (marks >= 40) {
+            grade = 'D';
+        } else {
+            grade = 'E';
+        }
+
+// Output student's grade
+        return 'getStudentGrade your marks is ${marks}${grade} ';
     }
 }
-
-// return
-checkSpeed(180);
-console.log();
-
-
+getStudentGrade();
